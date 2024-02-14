4d4cc1243c34ae5bd9d9016915721879cb1a4405
Rui Tong <torry_tong@outlook.com>
2024-02-13 18:10:11 -0800

    rt - create menuItemReview Fixtures

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,41 @@
+import { setAddon } from "@storybook/react";
+
+const menuItemReviewFixtures = {
+    oneReview: {
+        "id": 47,
+        "itemId": 7,
+        "reviewerEmail" : "cgaucho@ucsb.edu",
+        "stars": 5,
+        "comments": "I love the Apple Pie",
+        "dateReviewed": "2022-01-02T12:00:00"
+    },
+    threeReviews: [
+        {
+            "id": 47,
+            "itemId": 7,
+            "reviewerEmail" : "cgaucho@ucsb.edu",
+            "stars": 5,
+            "comments": "I love the Apple Pie",
+            "dateReviewed": "2022-01-02T12:00:00"
+        },
+        {
+            "id": 53,
+            "itemId": 7,
+            "reviewerEmail" : "ldelplaya@ucsb.edu",
+            "stars": 0,
+            "comments": "I hate the Apple Pie",
+            "dateReviewed": "2022-02-02T13:00:00"
+        },
+        {
+            "id": 1,
+            "itemId": 2,
+            "reviewerEmail" : "ldelplaya@ucsb.edu",
+            "stars": 5,
+            "comments": "I like the Banana Pie",
+            "dateReviewed": "2022-01-12T12:00:00"
+        }
+    ]
+};
+
+
+export { menuItemReviewFixtures };
