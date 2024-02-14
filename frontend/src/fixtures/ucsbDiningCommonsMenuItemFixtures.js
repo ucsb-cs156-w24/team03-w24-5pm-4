e891392d6fe3639b3e9337c18f3e51e27252c869
samanthwest <samanthawest@ucsb.edu>
2024-02-13 17:19:03 -0800

    sw - fix typo

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneDiningCommonsMenuItem: {
+        "id": 1,
+        "diningCommonsCode": "portola",
+        "name": "apple",
+        "station": "fruit"
+    },
+    threeDiningCommonsMenuItem: [
+        {
+            "id": 1,
+            "diningCommonsCode": "ortega",
+            "name": "pesto pasta",
+            "station": "entree"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "dlg",
+            "name": "french fries",
+            "station": "grill"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode": "carrillo",
+            "name": "pita chips",
+            "station": "deli"
+        }
+    ]
+};
+
+
+export { ucsbDiningCommonsMenuItemFixtures };
