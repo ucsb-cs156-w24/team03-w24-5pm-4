f7305ea417f9534b513726c3e1937f6a757b960c
Rui Tong <torry_tong@outlook.com>
2024-02-13 20:16:26 -0800

    solve conflicts

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -4,8 +4,8 @@ const menuItemReviewFixtures = {
         "itemId": 7,
         "reviewerEmail" : "cgaucho@ucsb.edu",
         "stars": 5,
-        "comments": "I love the Apple Pie",
-        "dateReviewed": "2022-01-02T12:00:00"
+        "dateReviewed": "2022-01-02T12:00:00",
+        "comments": "I love the Apple Pie"
     },
     threeReviews: [
         {
@@ -13,25 +13,25 @@ const menuItemReviewFixtures = {
             "itemId": 7,
             "reviewerEmail" : "cgaucho@ucsb.edu",
             "stars": 5,
-            "comments": "I love the Apple Pie",
-            "dateReviewed": "2022-01-02T12:00:00"
+            "dateReviewed": "2022-01-12T12:00:00",
+            "comments": "I love the Apple Pie"
         },
         {
             "id": 53,
             "itemId": 7,
             "reviewerEmail" : "ldelplaya@ucsb.edu",
             "stars": 0,
-            "comments": "I hate the Apple Pie",
-            "dateReviewed": "2022-02-02T13:00:00"
+            "dateReviewed": "2022-01-12T12:00:00",
+            "comments": "I hate the Apple Pie"  
         },
         {
             "id": 1,
             "itemId": 2,
             "reviewerEmail" : "ldelplaya@ucsb.edu",
             "stars": 5,
-            "comments": "I like the Banana Pie",
-            "dateReviewed": "2022-01-12T12:00:00"
-        }
+            "dateReviewed": "2022-01-12T12:00:00",
+            "comments": "I like the Banana Pie"
+        },
     ]
 };
 
