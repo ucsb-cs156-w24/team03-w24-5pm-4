6de4815178512271fab6efd6dfb4097a5812a75b
samanthwest <samanthawest@ucsb.edu>
2024-02-14 17:25:50 -0800

    sw - adding fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDatesFixtures = {
+    oneDate: {
+        "id": 1,
+        "name": "apple",
+        "station": "fruit",
+        "diningCommonsCode": "portola"
+    },
+    threeDates: [
+        {
+            "id": 1,
+            "name": "apple",
+            "station": "fruit",
+            "diningCommonsCode": "portola"
+        },
+        {
+            "id": 2,
+            "name": "pita chips",
+            "station": "deli",
+            "diningCommonsCode": "carrillo"
+        },
+        {
+            "id": 3,
+            "name": "french fries",
+            "station": "grill",
+            "diningCommonsCode": "dlg"
+        }
+    ]
+};
+
+
+export { ucsbDatesFixtures };
