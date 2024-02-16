a2f6404bdcb3fce4a8e7edbe68152002b022bf75
samanthwest <samanthawest@ucsb.edu>
2024-02-16 12:37:15 -0800

    sw - fixing edit table navigation typo

--- frontend/src/tests/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable.test.js
+++ frontend/src/tests/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable.test.js
@@ -115,7 +115,7 @@ describe("UserTable tests", () => {
     
     fireEvent.click(editButton);
 
-    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/ucsbdiningcommonsmenuitem/edit/2'));
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/diningcommonsmenuitem/edit/2'));
 
   });
   test("Delete button calls delete callback", async () => {
