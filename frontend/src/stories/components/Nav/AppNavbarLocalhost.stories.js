97484823f90c42a4565c002565a7782dab869b57
Megumi Ondo <megumiondo@Megumis-Air.hsd1.ca.comcast.net>
2024-02-18 22:01:54 -0800

    mo - fixing navbar

--- frontend/src/stories/components/Nav/AppNavbarLocalhost.stories.js
+++ frontend/src/stories/components/Nav/AppNavbarLocalhost.stories.js
@@ -1,22 +1,33 @@
 
-import React from 'react';
+import { Container, Nav, Navbar } from "react-bootstrap";
 
-import AppNavbarLocalhost from "main/components/Nav/AppNavbarLocalhost";
+export default function AppNavbarLocalhost({url}) {
+  return (
+    <>
+      <Navbar expand="sm" bg="warning" sticky="top" data-testid="AppNavbarLocalhost">
+        <Container>
+          <Navbar.Brand href="http://localhost:8080">
+            Warning
+          </Navbar.Brand>
 
-export default {
-    title: 'components/Nav/AppNavbarLocalhost',
-    component: AppNavbarLocalhost
-};
+          <Navbar.Toggle />
 
+          <Navbar.Collapse className="justify-content-between">
 
-const Template = (args) => {
-    return (
-        <AppNavbarLocalhost {...args} />
-    )
-};
-
-export const Default = Template.bind({});
-
+            <Nav className="mr-auto text-center">
+              <Nav.Item className="text-center">
+                <p>
+                  Running on <code>{ url }</code> with no backend.<br />
+                  You probably want <a href="http://localhost:8080">http://localhost:8080</a> instead.
+                </p>
+              </Nav.Item>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar >
+    </>
+  );
+}
 
 
 
