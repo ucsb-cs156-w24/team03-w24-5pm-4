52ae26b4c4986e74b96ea9fcc31cd6a7762852a9
Megumi Ondo <megumiondo@Megumis-Air.hsd1.ca.comcast.net>
2024-02-18 22:59:57 -0800

    mo - app.js file changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import UCSBOrganizationIndexPage from "main/pages/UCSBOrganization/UCSBOrganizat
 import UCSBOrganizationCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
 import UCSBOrganizationEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
 
+
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -89,14 +90,16 @@ function App() {
         {
           hasRole(currentUser, "ROLE_ADMIN") && (
             <>
-              <Route exact path="/ucsborganization/edit/:id" element={<UCSBOrganizationEditPage />} />
+              <Route exact path="/ucsborganization/edit/:orgCode" element={<UCSBOrganizationEditPage />} /> 
               <Route exact path="/ucsborganization/create" element={<UCSBOrganizationCreatePage />} />
             </>
           )
         }
+
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+// empty comment
