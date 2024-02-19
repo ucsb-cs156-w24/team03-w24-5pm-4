b4b404b1fc9e2c06c333fa7bdfe2adf486af20f5
anirudhiyer1 <97644421+anirudhiyer1@users.noreply.github.com>
2024-02-19 13:59:09 -0800

    Update PlaceholderIndexPage.js

--- frontend/src/main/pages/Placeholder/PlaceholderIndexPage.js
+++ frontend/src/main/pages/Placeholder/PlaceholderIndexPage.js
@@ -1,6 +1,6 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function MenuItemReviewIndexPage() {
+export default function PlaceHolderIndexPage() {
 
   // Stryker disable all : placeholder for future implementation
   return (
